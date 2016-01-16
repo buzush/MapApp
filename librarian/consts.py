885a8e3b937701de4a5908884a4a4ef226cba02d
Udi Oron <udioron@gmail.com>
2016-01-17 01:05:56 +0200

    Improving import from primo

--- librarian/consts.py
+++ librarian/consts.py
@@ -2,6 +2,7 @@ class MediaType:
     AUDIO = "audio"
     IMAGE = "image"
     ARCHIVE = "archive"
+    PHOTO = "photograph"
 
     MAP = "MAP"
     TRV = "TRV"
@@ -12,6 +13,7 @@ class MediaType:
         (AUDIO, "שיר"),
         (IMAGE, "תמונה"),
         (ARCHIVE, "פריט ארכיון"),
+        (PHOTO, "צילום"),
         (MAP, "מפה"),
         (TRV, "יומן מסע"),
         (VID, "קטע וידאו"),
